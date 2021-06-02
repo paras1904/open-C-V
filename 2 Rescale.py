0a0def70972286d08750965ea0b4c20e254db292
Paras rana <52912280+paras1904@users.noreply.github.com>
2021-06-02 15:03:02 +0530

    Add files via upload

---
+++ 2 Rescale.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+img = cv.imread('Chandler.jpg')
+cv.imread(',chandler',img)
+
+def rescaleFrame(frame,scale=0.75):
+    width = int(frame.shape[1]*scale)
+    height = int(frame.shape[0]*scale)
+    dimension = (width,height)
+    return cv.resize(frame,dimension,interpolation=cv.INTER_AREA )
+capture = cv.VideoCapture('Iron_M an.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Iron_man.mp4',frame)
+
+    frame_resized = rescaleFrame(frame,scale=.2)
+    cv.imshow('Iron_man_small',frame_resized)
+    if cv.waitKey(20) & 0xff == ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
