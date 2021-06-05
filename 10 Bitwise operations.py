b291837e45b0776440a80c7a50ed2c3125da5773
Paras rana <52912280+paras1904@users.noreply.github.com>
2021-06-05 11:09:27 +0530

    Add files via upload

--- 10 Bitwise operations.py
+++ 10 Bitwise operations.py
@@ -1,28 +1,12 @@
 import cv2 as cv
 import numpy as np
+img = cv.imread('Chandler.jpg')
+blank = np.zeros(img.shape[:2],dtype='uint8')
 
-blank = np.zeros((400,400),dtype='uint8')
-rectangle = cv.rectangle(blank.copy(),(30,30),(370,370),255,-1)
-circle = cv.circle(blank.copy(),(200,200),200,255,-1)
-cv.imshow('rectangle',rectangle)
-cv.imshow('circle',circle)
-
-
-# bitwise and
-bitwise_and = cv.bitwise_and(rectangle,circle)
-cv.imshow('and',bitwise_and)
-
-# bitwise or
-bitwise_or = cv.bitwise_or(circle,rectangle)
-cv.imshow('or',bitwise_or)
-
-# bitwise_xor
-bitwise_xor = cv.bitwise_xor(rectangle,circle)
-cv.imshow('xor',bitwise_xor)
-
-
-# bitwise_not
-bitwise_not = cv.bitwise_not(rectangle)
-cv.imshow('not',bitwise_not)
+# masking image
+mask = cv.circle(blank,(img.shape[1]//2,img.shape[0]//2),100,255,-1)
+cv.imshow('mask plain',mask)
+masked = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('masked',masked)
 
 cv.waitKey(0)
